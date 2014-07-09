7bcb71becf7c86e950b2967910f8f4ae24a3e79f
ZacharyAdams <zaekadams@gmail.com>
2014-07-09 17:18:15 -0400

    added basic crud for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,41 @@
 class PostsController < ApplicationController
    before_action :authenticate_user!, except: [:home]
+
   def index
+    @posts = Post.order(created_at: :desc).limit(10)
   end
 
   def home
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Success'
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if current_user?
+      @post = Post.find(params[:id])
+      if @post.destroy
+        flash[:notice] = 'Post deleted!'
+        redirect_to posts_path
+      end
+    end
+  end
+
+ private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :uid)
+  end
+
 end
