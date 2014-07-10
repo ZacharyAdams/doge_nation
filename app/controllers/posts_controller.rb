0184acc95437f5683116839f73a7de5db735fcc6
ZacharyAdams <zaekadams@gmail.com>
2014-07-10 13:51:07 -0400

    added crud functions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,17 @@ class PostsController < ApplicationController
   def home
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       flash[:notice] = 'Success'
       redirect_to posts_path
@@ -23,7 +28,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    if current_user?
+    if current_user
       @post = Post.find(params[:id])
       if @post.destroy
         flash[:notice] = 'Post deleted!'
@@ -32,7 +37,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    verify_authorized!(@post)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    verify_authorized!(@post)
+    if @post.update(post_params)
+      redirect_to(@post)
+    else
+      flash.now[:notice] = 'Much Fail, Very Sad, Edit Failed'
+      render :edit
+    end
+  end
+
  private
+
+  def verify_authorized!(post)
+    if current_user != post.user
+      flash[:notice] = 'You did not make this post! Random Shibe.'
+      redirect_to post_path
+    end
+  end
 
   def post_params
     params.require(:post).permit(:title, :body, :uid)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
-  validates :uid, presence: true
+  validates :user, presence: true
 belongs_to :user
 end
