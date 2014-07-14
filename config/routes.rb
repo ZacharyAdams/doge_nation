0e3c5570405b337ff195ecefcdfd2155072db561
ZacharyAdams <zaekadams@gmail.com>
2014-07-13 22:16:17 -0400

    fixed routing issues with staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root :to => 'static_pages#home'
-  get '/static_pages/home', :to =>'static_pages#home'
-  get 'static_pages/home'
+  root :to => 'static_pages#index'
+  resources :static_pages, only: [:index]
   devise_for :users
   resources :posts do
         resources :replies, only: [:create, :new, :destroy, :edit, :update]
